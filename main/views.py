d3d9eb44fc966a18576cc39069056246269e45bd
VasanthBalasubramaniyan <vambuvasanth124@gmail.com>
2025-04-10 18:56:58 +0530

    Created app

---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def blog_home(request):
+    return render(request,'main/blog_home.html')
+def blog_detail(request):
+    return render(request,'main/blog_detail.html')
+def profile(request):
+    return render(request,'main/profile.html')
